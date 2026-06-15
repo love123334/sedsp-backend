f9360d443b3a8c5f5b3ced09174b66d40381b98d
LGTNT <155623585+linhtnt2004@users.noreply.github.com>
2026-06-15 14:53:21 +0700

    "feat: add wishlist table"

---
+++ src/main/resources/db/migration/V21__add_wishlist_management.sql
@@ -0,0 +1,37 @@
+CREATE TABLE wishlists
+(
+    id BIGSERIAL PRIMARY KEY,
+
+    customer_id BIGINT NOT NULL UNIQUE,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_wishlist_customer
+        FOREIGN KEY (customer_id)
+            REFERENCES customers(id)
+            ON DELETE CASCADE
+);
+
+CREATE TABLE wishlist_items
+(
+    id BIGSERIAL PRIMARY KEY,
+
+    wishlist_id BIGINT NOT NULL,
+
+    product_id BIGINT NOT NULL,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_wishlist_item_wishlist
+        FOREIGN KEY (wishlist_id)
+            REFERENCES wishlists(id)
+            ON DELETE CASCADE,
+
+    CONSTRAINT fk_wishlist_item_product
+        FOREIGN KEY (product_id)
+            REFERENCES products(id)
+            ON DELETE CASCADE,
+
+    CONSTRAINT uq_wishlist_product
+        UNIQUE (wishlist_id, product_id)
+);
