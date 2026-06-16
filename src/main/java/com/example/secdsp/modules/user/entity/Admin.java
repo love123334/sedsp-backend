e7aa86f7091ade61ff7ea86a9efa7c102e4f8aed
LGTNT <155623585+linhtnt2004@users.noreply.github.com>
2026-06-17 01:28:24 +0700

    "fix: create admin, manager, seller, customer class"

---
+++ src/main/java/com/example/secdsp/modules/user/entity/Admin.java
@@ -0,0 +1,30 @@
+package com.example.secdsp.modules.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "admins")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "admin_level")
+    private Integer adminLevel;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
