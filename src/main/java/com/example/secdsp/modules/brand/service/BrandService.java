c1000ab1bb3f40ea8293f7e59abe3616ec06d756
LGTNT <155623585+linhtnt2004@users.noreply.github.com>
2026-06-18 08:18:47 +0700

    "feat: add method findEntityById"

--- src/main/java/com/example/secdsp/modules/brand/service/BrandService.java
+++ src/main/java/com/example/secdsp/modules/brand/service/BrandService.java
@@ -3,8 +3,11 @@ package com.example.secdsp.modules.brand.service;
 import com.example.secdsp.modules.brand.dto.request.CreateBrandRequest;
 import com.example.secdsp.modules.brand.dto.request.UpdateBrandRequest;
 import com.example.secdsp.modules.brand.dto.response.BrandResponse;
+import com.example.secdsp.modules.brand.entity.Brand;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
 
 public interface BrandService {
     BrandResponse createBrand(CreateBrandRequest request);
@@ -12,4 +15,5 @@ public interface BrandService {
     void deleteBrand(Long id);
     BrandResponse getBrandById(Long id);
     Page<BrandResponse> getBrands(String keyword, Pageable pageable);
+    Optional<Brand> findEntityById(Long id);
 }
