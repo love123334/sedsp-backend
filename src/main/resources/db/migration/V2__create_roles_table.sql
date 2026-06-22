fc52a85accd8178313df7eecc0a985c70d64000e
LGTNT <155623585+linhtnt2004@users.noreply.github.com>
2026-06-22 10:04:21 +0700

    "feat: add update and delete at to V2_create_roles_table.sql"

--- src/main/resources/db/migration/V2__create_roles_table.sql
+++ src/main/resources/db/migration/V2__create_roles_table.sql
@@ -3,12 +3,13 @@ CREATE TABLE roles
     id          BIGSERIAL PRIMARY KEY,
     name        VARCHAR(50) NOT NULL UNIQUE,
     description TEXT,
-    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at  TIMESTAMP
 );
 
 INSERT INTO roles (name, description)
-VALUES
-    ('ADMIN', 'System Administrator'),
-    ('MANAGER', 'Business Manager'),
-    ('SELLER', 'Product Seller'),
-    ('CUSTOMER', 'Customer User');
+VALUES ('ADMIN', 'System Administrator'),
+       ('MANAGER', 'Business Manager'),
+       ('SELLER', 'Product Seller'),
+       ('CUSTOMER', 'Customer User');
