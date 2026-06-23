de3cc847e53ea9589b869825c244aa51a7282300
LGTNT <155623585+linhtnt2004@users.noreply.github.com>
2026-06-24 00:15:06 +0700

    "fix: reformat and fix seller_description type"

--- src/main/resources/db/migration/V3__create_users_table.sql
+++ src/main/resources/db/migration/V3__create_users_table.sql
@@ -1,28 +1,28 @@
 CREATE TABLE users
 (
-    id         BIGSERIAL PRIMARY KEY,
+    id             BIGSERIAL PRIMARY KEY,
 
-    username   VARCHAR(100) NOT NULL UNIQUE,
-    email      VARCHAR(150) NOT NULL UNIQUE,
-    password   VARCHAR(255) NOT NULL,
+    username       VARCHAR(100) NOT NULL UNIQUE,
+    email          VARCHAR(150) NOT NULL UNIQUE,
+    password       VARCHAR(255) NOT NULL,
 
-    full_name  VARCHAR(150),
-    phone      VARCHAR(20) UNIQUE,
+    full_name      VARCHAR(150),
+    phone          VARCHAR(20) UNIQUE,
 
-    status     user_status DEFAULT 'ACTIVE'::user_status,
+    status         user_status DEFAULT 'ACTIVE'::user_status,
 
-    role_id    BIGINT NOT NULL
-        REFERENCES roles(id),
+    role_id        BIGINT NOT NULL
+        REFERENCES roles (id),
 
-    store_name VARCHAR(255),
+    store_name     VARCHAR(255),
     business_email VARCHAR(150),
     business_phone VARCHAR(20),
-    seller_description ,
+    seller_description TEXT,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP
+    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at     TIMESTAMP
 );
 
 CREATE INDEX idx_users_role
-    ON users(role_id);
+    ON users (role_id);
