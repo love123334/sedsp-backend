4c39d1db18f10fb1f4df43e16a2d5c8251c0af72
LGTNT <155623585+linhtnt2004@users.noreply.github.com>
2026-06-24 16:33:52 +0700

    "feat: edit Category entity (use SQLDelete and SQLRestriction)"

--- src/main/java/com/example/secdsp/modules/category/entity/Category.java
+++ src/main/java/com/example/secdsp/modules/category/entity/Category.java
@@ -7,9 +7,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -18,25 +18,33 @@ import java.util.List;
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @NoArgsConstructor
+@SQLDelete(
+    sql = """
+            UPDATE categories
+            SET deleted_at = CURRENT_TIMESTAMP
+            WHERE id = ?
+        """
+)
+@SQLRestriction("deleted_at IS NULL")
 public class Category extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-     Long id;
+    Long id;
+
+    @Column(nullable = false, length = 150)
+    String name;
 
     @Column(nullable = false, unique = true, length = 150)
-     String name;
-
-    @Column(nullable = false, unique = true, length = 150)
-     String slug;
+    String slug;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
-     Category parent;
+    Category parent;
 
     @OneToMany(
         mappedBy = "parent",
         fetch = FetchType.LAZY
     )
-     List<Category> children;
+    List<Category> children;
 }
