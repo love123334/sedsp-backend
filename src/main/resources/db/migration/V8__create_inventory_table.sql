df2b69cf9967f4bc00917054f130838379d476c0
LGTNT <155623585+linhtnt2004@users.noreply.github.com>
2026-06-27 02:40:57 +0700

    "feat: update inventory and inventory_logs tables"

--- src/main/resources/db/migration/V8__create_inventory_table.sql
+++ src/main/resources/db/migration/V8__create_inventory_table.sql
@@ -1,15 +1,16 @@
 CREATE TABLE inventory
 (
-    id                   BIGSERIAL PRIMARY KEY,
+    id                 BIGSERIAL PRIMARY KEY,
 
-    product_id           BIGINT NOT NULL UNIQUE
-        REFERENCES products(id)
+    product_id         BIGINT    NOT NULL UNIQUE
+        REFERENCES products (id)
             ON DELETE CASCADE,
 
-    available_quantity   INTEGER DEFAULT 0,
-    reserved_quantity    INTEGER DEFAULT 0,
+    available_quantity INTEGER   NOT NULL DEFAULT 0,
 
-    updated_at           TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    reserved_quantity  INTEGER   NOT NULL DEFAULT 0,
+
+    updated_at         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT chk_inventory_available
         CHECK (available_quantity >= 0),
--- src/main/resources/db/migration/V9__create_inventory_logs_table.sql
+++ src/main/resources/db/migration/V9__create_inventory_logs_table.sql
@@ -1,17 +1,29 @@
 CREATE TABLE inventory_logs
 (
-    id               BIGSERIAL PRIMARY KEY,
+    id                BIGSERIAL PRIMARY KEY,
 
-    product_id       BIGINT NOT NULL
-        REFERENCES products(id)
+    product_id        BIGINT               NOT NULL
+        REFERENCES products (id)
             ON DELETE CASCADE,
 
-    change_amount    INTEGER NOT NULL,
+    change_amount     INTEGER              NOT NULL,
 
-    reason           VARCHAR(255),
+    previous_quantity INTEGER              NOT NULL,
+    current_quantity  INTEGER              NOT NULL,
 
-    created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    reason            inventory_log_reason NOT NULL,
+
+    updated_by        BIGINT
+        REFERENCES users (id),
+
+    created_at        TIMESTAMP            NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT chk_inventory_log_current_non_negative
+        CHECK (current_quantity >= 0)
 );
 
 CREATE INDEX idx_inventory_logs_product
     ON inventory_logs(product_id);
+
+CREATE INDEX idx_inventory_logs_updated_by
+    ON inventory_logs(updated_by);
