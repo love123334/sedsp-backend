bf5e8559f7cb22d5033c6ce775af602afef911cc
LGTNT <155623585+linhtnt2004@users.noreply.github.com>
2026-07-02 08:37:01 +0700

    "feat: create seller dashboard controller"

---
+++ src/main/java/com/example/secdsp/modules/sellerdashboard/controller/SellerDashboardController.java
@@ -0,0 +1,31 @@
+package com.example.secdsp.modules.sellerdashboard.controller;
+
+import com.example.secdsp.common.api.ApiResponse;
+import com.example.secdsp.modules.sellerdashboard.dto.SellerDashboardResponse;
+import com.example.secdsp.modules.sellerdashboard.service.SellerDashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/seller/dashboard")
+@RequiredArgsConstructor
+@PreAuthorize("hasRole('SELLER')")
+public class SellerDashboardController {
+
+    private final SellerDashboardService dashboardService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<SellerDashboardResponse>>
+    getDashboard() {
+
+        return ResponseEntity.ok(
+            ApiResponse.success(
+                dashboardService.getDashboard()
+            )
+        );
+    }
+}
