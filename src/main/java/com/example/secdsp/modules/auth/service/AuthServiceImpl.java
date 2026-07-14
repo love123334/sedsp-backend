3168e72d1b9381bf2bcae103b75260b320387673
LGTNT <155623585+linhtnt2004@users.noreply.github.com>
2026-07-14 10:12:32 +0700

    "feat: add new method in AuthService"

--- src/main/java/com/example/secdsp/modules/auth/service/AuthServiceImpl.java
+++ src/main/java/com/example/secdsp/modules/auth/service/AuthServiceImpl.java
@@ -9,6 +9,10 @@ import com.example.secdsp.modules.auth.dto.request.RegisterRequest;
 import com.example.secdsp.modules.auth.dto.response.LoginResponse;
 import com.example.secdsp.modules.auth.dto.response.MeResponse;
 import com.example.secdsp.modules.auth.mapper.AuthMapper;
+import com.example.secdsp.modules.email.dto.request.UpdatePasswordRequest;
+import com.example.secdsp.modules.email.dto.request.VerifyOtpRequest;
+import com.example.secdsp.modules.email.entity.EmailOtp;
+import com.example.secdsp.modules.email.repository.EmailOtpRepository;
 import com.example.secdsp.modules.email.service.EmailService;
 import com.example.secdsp.modules.email.service.OtpService;
 import com.example.secdsp.modules.user.entity.Role;
@@ -29,6 +33,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -42,6 +48,7 @@ public class AuthServiceImpl implements AuthService {
     private final PasswordEncoder passwordEncoder;
     private final OtpService otpService;
     private final EmailService emailService;
+    private final EmailOtpRepository emailOtpRepository;
 
     @Value("${app.jwt.expiration-ms}")
     private long jwtExpirationMs;
@@ -171,5 +178,84 @@ public class AuthServiceImpl implements AuthService {
         String otp = otpService.resendOtp(email);
 
         emailService.sendOtp(email, otp);
+    }
+
+    @Override
+    @Transactional
+    public void forgotPassword(String email) {
+
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() ->
+                             new BusinessException("Email does not exist", HttpStatus.BAD_REQUEST));
+
+        if (user.getStatus() == UserStatus.BLOCKED) {
+            throw new BusinessException(
+                "Account is blocked",
+                HttpStatus.FORBIDDEN
+            );
+        }
+
+        if (user.getStatus() != UserStatus.ACTIVE) {
+            throw new BusinessException(
+                "Account is not active",
+                HttpStatus.BAD_REQUEST
+            );
+        }
+
+        String otp = otpService.generateOtp(email);
+
+        emailService.sendResetPasswordOtp(email, otp);
+    }
+
+    @Override
+    @Transactional
+    public void verifyResetOtp(VerifyOtpRequest request) {
+
+        User user = userRepository.findByEmail(request.getEmail())
+            .orElseThrow(() ->
+                             new BusinessException("Invalid request", HttpStatus.BAD_REQUEST));
+
+        otpService.validateOtp(request.getEmail(), request.getOtp());
+
+    }
+
+    @Override
+    @Transactional
+    public void updatePassword(UpdatePasswordRequest request) {
+
+        if (!request.getNewPassword()
+            .equals(request.getConfirmPassword())) {
+
+            throw new BusinessException(
+                "Password confirmation does not match",
+                HttpStatus.BAD_REQUEST
+            );
+        }
+
+        User user = userRepository.findByEmail(request.getEmail())
+            .orElseThrow(() ->
+                             new BusinessException("Invalid request", HttpStatus.BAD_REQUEST));
+
+        EmailOtp latestOtp = emailOtpRepository
+            .findTopByEmailOrderByIdDesc(request.getEmail())
+            .orElseThrow(() ->
+                             new BusinessException("Invalid request", HttpStatus.BAD_REQUEST));
+
+        if (!latestOtp.isVerified()) {
+            throw new BusinessException(
+                "OTP verification required",
+                HttpStatus.BAD_REQUEST
+            );
+        }
+
+        if (latestOtp.getExpiryTime().isBefore(LocalDateTime.now())) {
+            throw new BusinessException("OTP expired", HttpStatus.BAD_REQUEST);
+        }
+
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+        userRepository.save(user);
+
+        latestOtp.setVerified(false);
+        emailOtpRepository.save(latestOtp);
     }
 }
