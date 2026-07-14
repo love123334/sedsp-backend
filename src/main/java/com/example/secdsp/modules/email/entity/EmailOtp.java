2895f24eb3b0c18b5e6823845532bd95915433e6
LGTNT <155623585+linhtnt2004@users.noreply.github.com>
2026-07-14 09:02:54 +0700

    "feat: create EmailOtp entity"

---
+++ src/main/java/com/example/secdsp/modules/email/entity/EmailOtp.java
@@ -0,0 +1,35 @@
+package com.example.secdsp.modules.email.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "email_otps")
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class EmailOtp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String email;
+
+    String otp;
+
+    LocalDateTime expiryTime;
+
+    boolean used;
+
+    @Column(nullable = false)
+    LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    int resendCount;
+}
