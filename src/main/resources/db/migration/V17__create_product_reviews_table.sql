9f9b853c926811d35e248f604e23d4bb1847c767
LGTNT <155623585+linhtnt2004@users.noreply.github.com>
2026-07-20 17:50:35 +0700

    "feat: update product review table"

--- src/main/resources/db/migration/V17__create_product_reviews_table.sql
+++ src/main/resources/db/migration/V17__create_product_reviews_table.sql
@@ -7,20 +7,24 @@ CREATE TABLE product_reviews
             ON DELETE CASCADE,
 
     user_id        BIGINT NOT NULL
-        REFERENCES users(id),
+        REFERENCES users(id)
+            ON DELETE CASCADE,
 
     rating         INTEGER NOT NULL,
 
     comment        TEXT,
 
-    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at     TIMESTAMP,
 
     CONSTRAINT chk_review_rating
-        CHECK (rating BETWEEN 1 AND 5),
-
-    CONSTRAINT uq_review_user_product
-        UNIQUE(user_id, product_id)
+        CHECK (rating BETWEEN 1 AND 5)
 );
+
+CREATE UNIQUE INDEX ux_review_user_product
+    ON product_reviews(user_id, product_id)
+    WHERE deleted_at IS NULL;
 
 CREATE INDEX idx_reviews_product
     ON product_reviews(product_id);
