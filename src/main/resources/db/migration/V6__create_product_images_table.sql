a14c68b24cbe708c3f915a1c03b6f77c35b170ee
LGTNT <155623585+linhtnt2004@users.noreply.github.com>
2026-07-20 16:24:47 +0700

    "feat: update product image table"

--- src/main/resources/db/migration/V6__create_product_images_table.sql
+++ src/main/resources/db/migration/V6__create_product_images_table.sql
@@ -1,18 +1,20 @@
 CREATE TABLE product_images
 (
-    id         BIGSERIAL PRIMARY KEY,
+    id          BIGSERIAL PRIMARY KEY,
 
-    product_id BIGINT NOT NULL
+    product_id  BIGINT       NOT NULL
         REFERENCES products (id)
             ON DELETE CASCADE,
 
-    image_url  TEXT   NOT NULL,
+    image_url   TEXT         NOT NULL,
 
-    is_primary BOOLEAN   DEFAULT FALSE,
+    public_id   VARCHAR(255) NOT NULL,
 
-    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP
+    is_primary  BOOLEAN      NOT NULL DEFAULT FALSE,
+
+    created_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at  TIMESTAMP
 );
 
 CREATE INDEX idx_product_images_product
